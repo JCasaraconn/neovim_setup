826d12ab5551857372f3af1766f513e03674a3d6
Josh Casara <jcasara@gmail.com>
2025-01-07 09:31:22 -0500

    feat: add black mypy and pylint to none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,12 +5,14 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,     -- Lua formatter
-        -- null_ls.builtins.formatting.autopep8,   -- Python formatter
+        null_ls.builtins.formatting.black,   -- Python formatter
         null_ls.builtins.formatting.isort,      -- Python import sorter
         null_ls.builtins.formatting.prettier,   -- Formatter for various files
         -- null_ls.builtins.formatting.beautysh,   -- Shell script formatter
         -- null_ls.builtins.diagnostics.flake8,    -- Python linter
         null_ls.builtins.diagnostics.yamllint,  -- YAML linter
+        null_ls.builtins.diagnostics.mypy, -- Python typechecker
+        null_ls.builtins.diagnostics.pylint, -- Python linter
         -- null_ls.builtins.diagnostics.shellcheck -- Shell script linter
       },
     })
