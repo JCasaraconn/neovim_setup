fab9845b26ca514eb2c13b8b9c121d114f5a907b
Josh Casara <jcasara@gmail.com>
2025-01-28 11:36:25 -0800

    feat: add jedi language server support for python files

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -69,4 +69,25 @@ return {
 			})
 		end,
 	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- Configure jedi-language-server
+			lspconfig.jedi_language_server.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					-- Example: Keymaps for LSP navigation
+					local opts = { noremap = true, silent = true, buffer = bufnr }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
+				end,
+			})
+		end,
+	},
 }
