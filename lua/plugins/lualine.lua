d00f69f81e6362a48be72e269ade2b8b2bc1283e
Josh Casaraconn <jcasaraconn@gmail.com>
2026-09-17 01:07:04 -0400

    refactor: let lualine follow the colorscheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,11 +3,7 @@ return {
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy", -- load this after critical plugins
 		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "catppuccin-nvim",
-				},
-			})
+			require("lualine").setup()
 		end,
 	},
 }
